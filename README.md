72bff0e6abdc95b147419982a661378aed0a2f06
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2022-08-30 17:24:34 -0400

    setup project and files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # genetic-algorithm
+
 a genetic algorithm try in ts - http://www.ai-junkie.com/ga/intro/gat1.html
+
+# to run: npm start
+
+# to watch: npm run watch
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+// Given the digits 0 through 9 and the operators +, -, * and /,  find a sequence that will represent a given target number. The operators will be applied sequentially from left to right as you read.
+
+type Bit = number
+type Gene = [Bit]
+type Chromossome = [Gene]
+
+console.log('Hello')
